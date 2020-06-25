cb4bc863f5765773c2fffd1bca7136ea524f58d7
kavindagk <kavindagk@gmail.com>
2020-06-25 13:33:58 +0530

    1st chart completed

--- plot1.R
+++ plot1.R
@@ -1,9 +1,16 @@
+#Data read and store in to data frame
 dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#filter subset
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 
+#extract required fields
+globalActivePower <- as.numeric(subSetData$Global_active_power)
 
-#ggplot
-ggplot(data=subSetData,aes(as.numeric(Global_active_power))) + geom_histogram(bins = 10)
 
+#Crete PNG file
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
